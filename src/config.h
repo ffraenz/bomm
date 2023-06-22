d9116391c58fc65b2ba03623e6000d73a0503927
Fränz Friederes <fraenz@frieder.es>
2023-06-22 18:28:28 +0200

    Attempt to fix 'variably modified at file scope' CI issue

--- src/config.h
+++ src/config.h
@@ -8,12 +8,13 @@
 #ifndef config_h
 #define config_h
 
+#include <string.h>
 #define BOMM_ALPHABET_LATIN "abcdefghijklmnopqrstuvwxyz"
 
 #ifndef BOMM_ALPHABET
     #define BOMM_ALPHABET BOMM_ALPHABET_LATIN
 #endif
 
-#define BOMM_ALPHABET_SIZE ((unsigned int) strlen(BOMM_ALPHABET))
+#define BOMM_ALPHABET_SIZE ((unsigned int) (sizeof BOMM_ALPHABET - 1))
 
 #endif /* config_h */
